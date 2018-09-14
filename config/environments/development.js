046bfa9e54d1f13ea5fa354e3737acb4ebb3dbc7
xiaofeng <xiaofeng@cnbn.cn>
2018-09-14 17:30:29 +0800

    增加相关配置预留项

--- config/environments/development.js
+++ config/environments/development.js
@@ -2,11 +2,29 @@
 
 module.exports = {
   
+  // 设置时区
+  time_zone: 'Beijing',
+  
+  // 设置本地化
+  default_locale = "zh-CN",
+  
   // 日志级别
-  debug_level: 'debug'
+  debug_level: 'debug',
+  
+  // 类的自动加载路径
+  autorequire_paths: ['app', 'lib'],
+  
+  // 是否缓存类，生产环境应为true
+  cache_classes: false,
+  
+  // 是否在boot.js中加载所有类, 生产环境应为true
+  eager_load: false
   
   
+  // TODO: 视图页配置app/views
   
+  
+  // TODO: 邮件相关配置
   
   
   
