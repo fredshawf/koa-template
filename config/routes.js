973694e21fd64bc83ce46e283d79393f6b5a4782
fredxiaoF <gift5168xiaofeng@qq.com>
2018-10-16 23:43:21 +0800

    路由初始化

--- config/routes.js
+++ config/routes.js
@@ -1,7 +1,42 @@
 const Router = require('koa-router');
+const RouterGenerator = require('./koa-router-generator');
 
-module.exports = Koa.app.router = new Router();
+const router = new Router();
+module.exports = Koa.app.router = router
 
 
-Koa.app.router.get('/abc/:id/:name', BaseController.action);
+// Koa.app.router.get('/abc/:id/:name', BaseController.action);
 
+RouterGenerator.draw(router, function(generator) {
+  
+  //1 TODO: namespace
+  this.namespace('abc', function(space){
+    
+    //2 TODO: resource with opts <controller action namespace (only except)>
+    this.resources('application', {controller: 'ApplicationController'}, function(){
+      //3 TODO: member/collection 
+      this.member(function(){
+        this.get('adssdf')
+      })
+    })
+    
+    //4 TODO: 普通的基于koa-router的重写
+    this.get('/path', 'Application#index');
+    this.post('/path/:id', 'Application#create');
+    
+  })
+  
+  //5 TODO: 普通的基于koa-router的重写
+  this.get('/path', 'Application#index');
+  this.post('/path/:id', 'Application#create');
+  
+  //6 TODO: 基于controller action写法
+  this.all('/:controller/:action')
+  
+  
+  // 纯koa-router写法
+  router.get('/abc', async () => {})
+  
+  
+  
+})
