f2a3eeba13d2994609f9ed4044cc88a7c5257f26
fredxiaoF <gift5168xiaofeng@qq.com>
2018-10-22 18:06:57 +0800

    update

--- config/koa-router-dispatcher.js
+++ config/koa-router-dispatcher.js
@@ -1,16 +1,20 @@
 module.exports = class RouterDispatcher {
   
-  constructor(){
-    
-    
-  }
   
   
   static async call(ctx) {
     let dispatcher = new this(ctx);
-    
-    
-    
+    let controller = ctx.params.controller
+    let action = ctx.params.action
+    await this.dispatch(controller, action, ctx);
+  }
+  
+  
+  
+  static async dispatch(controller, action, ctx) {
+    let controller_class = eval(controller);
+    let controller = new controller_class(ctx);
+    await controller[action].apply(controller);
   }
   
   
